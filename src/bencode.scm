c25937d6446151c526bba692f8a66d63415a8f9d
Andrew Tropin <andrew@trop.in>
2023-05-11 13:27:13 +0400

    bencode: Add integer encoding and decoding

--- src/bencode.scm
+++ src/bencode.scm
@@ -1,11 +1,60 @@
 (define-module (bencode)
   #:export (scm->bencode
-            scm->bencode-string))
+            scm->bencode-string
+            bencode->scm
+            bencode-string->scm))
+
+(define (bencode-exception port)
+  (throw 'bencode-invalid port))
+
+(define (integer->bencode int)
+  (format #f "i~ae" int))
 
 (define* (scm->bencode scm #:optional (port (current-output-port)))
-  (display "hi" port))
+  (display (integer->bencode scm) port))
 
 (define* (scm->bencode-string scm)
   (call-with-output-string
    (lambda (p)
      (scm->bencode scm p))))
+
+(define (expect-char pred port)
+  (let ((ch (peek-char port)))
+    (cond
+     ((not (pred ch)) (bencode-exception port))
+     ((eof-object? ch) (bencode-exception port)))))
+
+(define (digit? c)
+  (case c
+    ((#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9) #t)
+    (else #f)))
+
+(define (read-bencode-integer port)
+  (expect-char (lambda (x) (char=? #\i x)) port)
+  (read-char port)
+  (let loop ((ch (read-char port))
+             (res ""))
+    (cond ((and (char=? #\e ch)
+                (not (string-null? res))
+                (not (string=? res "-")))
+           (string->number res))
+
+          ((or
+            (digit? ch)
+            (and (char=? #\- ch) (string-null? res)))
+           (loop (read-char port) (string-append res (string ch))))
+
+          (else
+           (bencode-exception port)))))
+
+(define* (bencode->scm #:optional (port (current-input-port)))
+  ;; (peek-char )
+  (read-bencode-integer port)
+  )
+
+(define* (bencode-string->scm str)
+  (call-with-input-string str
+    (lambda (p)
+      (bencode->scm p))))
+
+;; (bencode-string->scm "i43e")
--- tests/bencode-test.scm
+++ tests/bencode-test.scm
@@ -14,3 +14,14 @@
     (test-equal "0" "i0e" (scm->bencode-string 0))
     (test-equal "42" "i42e" (scm->bencode-string 42))
     (test-equal "-42" "i-42e" (scm->bencode-string -42))))
+
+(define-test decode
+  (test-group "integers"
+    (test-equal "i0e :: zero" 0 (bencode-string->scm "i0e"))
+    (test-error "i0a :: unexpected character" #t (bencode-string->scm "i0a"))
+    (test-equal "i42e :: positive number" 42 (bencode-string->scm "i42e"))
+    (test-equal "i-42e :: negative number" -42 (bencode-string->scm "i-42e"))
+    (test-error "i4-2e :: unexpected character" #t (bencode-string->scm "i4-2e"))
+    (test-error "i--4e :: unexpected character" #t (bencode-string->scm "i--4e"))
+    (test-error "ie :: no digits" #t (bencode-string->scm "ie"))
+    (test-error "i-e :: no digits" #t (bencode-string->scm "i-e"))))
