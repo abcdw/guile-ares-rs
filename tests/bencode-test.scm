cefb3ec3d93804d53f225fcdacc2682045e280a1
Andrew Tropin <andrew@trop.in>
2023-05-12 08:12:16 +0400

    tests: Add wrapping test groups and strings decoding tests

--- tests/bencode-test.scm
+++ tests/bencode-test.scm
@@ -10,46 +10,58 @@
        (set-procedure-property! test-name 'srfi-64-test? #t)))))
 
 (define-test encode
-  (test-group "integers"
-    (test-equal "0" "i0e" (scm->bencode-string 0))
-    (test-equal "42" "i42e" (scm->bencode-string 42))
-    (test-equal "-42" "i-42e" (scm->bencode-string -42)))
+  (test-group "encode"
+    (test-group "integers"
+      (test-equal "0" "i0e" (scm->bencode-string 0))
+      (test-equal "42" "i42e" (scm->bencode-string 42))
+      (test-equal "-42" "i-42e" (scm->bencode-string -42)))
 
-  (test-group "strings"
-    (test-equal "\"spam\"" "4:spam" (scm->bencode-string "spam"))
-    (test-equal "empty string" "0:" (scm->bencode-string "")))
+    (test-group "strings"
+      (test-equal "\"spam\"" "4:spam" (scm->bencode-string "spam"))
+      (test-equal "empty string" "0:" (scm->bencode-string "")))
 
-  (test-group "lists"
-    (test-equal "empty list" "le" (scm->bencode-string #()))
-    (test-equal "list with number and string"
-      "l4:spami-42ee" (scm->bencode-string #("spam" -42)))
-    (test-equal "list with nested list, number and string"
-      "ll4:spamelei-42ee" (scm->bencode-string #(#("spam") #() -42))))
+    (test-group "lists"
+      (test-equal "empty list" "le" (scm->bencode-string #()))
+      (test-equal "list with number and string"
+        "l4:spami-42ee" (scm->bencode-string #("spam" -42)))
+      (test-equal "list with nested list, number and string"
+        "ll4:spamelei-42ee" (scm->bencode-string #(#("spam") #() -42))))
 
-  (test-group "dictionaries"
-    (test-equal "empty dict" "de" (scm->bencode-string '()))
-    (test-equal "dict with number and string"
-      "d4:spami-42ee" (scm->bencode-string '(("spam" . -42))))
-    (test-error
-     "dict with non-string key"
-     #t (scm->bencode-string '((42 . -42))))
-    (test-equal "dict without value"
-     "d4:listde4:spami-42ee" (scm->bencode-string '(("list")
-                                                    ("spam" . -42))))
-    (test-error
-     "malformed dict with empty key"
-     #t (scm->bencode-string '(("spam") ())))
-    (test-error
-     "malformed dict with number"
-     #t (scm->bencode-string '(("spam") -42)))))
+    (test-group "dictionaries"
+      (test-equal "empty dict" "de" (scm->bencode-string '()))
+      (test-equal "dict with number and string"
+        "d4:spami-42ee" (scm->bencode-string '(("spam" . -42))))
+      (test-error
+       "dict with non-string key"
+       #t (scm->bencode-string '((42 . -42))))
+      (test-equal "dict without value"
+        "d4:listde4:spami-42ee" (scm->bencode-string '(("list")
+                                                       ("spam" . -42))))
+      (test-error
+       "malformed dict with empty key"
+       #t (scm->bencode-string '(("spam") ())))
+      (test-error
+       "malformed dict with number"
+       #t (scm->bencode-string '(("spam") -42))))))
 
 (define-test decode
-  (test-group "integers"
-    (test-equal "i0e :: zero" 0 (bencode-string->scm "i0e"))
-    (test-error "i0a :: unexpected character" #t (bencode-string->scm "i0a"))
-    (test-equal "i42e :: positive number" 42 (bencode-string->scm "i42e"))
-    (test-equal "i-42e :: negative number" -42 (bencode-string->scm "i-42e"))
-    (test-error "i4-2e :: unexpected character" #t (bencode-string->scm "i4-2e"))
-    (test-error "i--4e :: unexpected character" #t (bencode-string->scm "i--4e"))
-    (test-error "ie :: no digits" #t (bencode-string->scm "ie"))
-    (test-error "i-e :: no digits" #t (bencode-string->scm "i-e"))))
+  (test-group "decode"
+    (test-group "integers"
+      (test-equal "i0e :: zero" 0 (bencode-string->scm "i0e"))
+      (test-error "i0a :: unexpected character" #t (bencode-string->scm "i0a"))
+      (test-equal "i42e :: positive number" 42 (bencode-string->scm "i42e"))
+      (test-equal "i-42e :: negative number" -42 (bencode-string->scm "i-42e"))
+      (test-error "i4-2e :: unexpected character" #t (bencode-string->scm "i4-2e"))
+      (test-error "i--4e :: unexpected character" #t (bencode-string->scm "i--4e"))
+      (test-error "ie :: no digits" #t (bencode-string->scm "ie"))
+      (test-error "i-e :: no digits" #t (bencode-string->scm "i-e"))
+      (test-error "i :: unexpcted EOF" #t (bencode-string->scm "i"))
+      (test-error "i-12 :: unexpcted EOF" #t (bencode-string->scm "i-12")))
+
+    (test-group "strings"
+      (test-equal "0: :: empty string"
+        "" (bencode-string->scm "0:"))
+      (test-equal "4:spam :: simple string"
+        "spam" (bencode-string->scm "4:spam"))
+      (test-error "2:sp2:am :: two strings" #t (bencode-string->scm "2:sp2:am"))
+      (test-error "4:spa :: unexpected EOF" #t (bencode-string->scm "4:spa")))))
