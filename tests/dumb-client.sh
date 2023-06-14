0ad096d20b1cfbc901568dc3562712964405e0fd
Andrew Tropin <andrew@trop.in>
2023-06-14 11:32:09 +0400

    tests: Add id to ops

--- tests/dumb-client.sh
+++ tests/dumb-client.sh
@@ -1,5 +1,5 @@
 NC=`guix build netcat-openbsd`/bin/nc
 
 # printf "(+ 1 2)\n" | $NC -v -q 0 127.0.0.1 11211
-printf "d2:op4:eval4:code1:+e" | $NC -v -q 0 127.0.0.1 ${1:-7888}
-printf "d2:op4:eval4:code1:ee" | $NC -v -N 127.0.0.1 ${1:-7888}
+printf "d2:id1:12:op4:eval4:code1:+e" | $NC -v -q 0 127.0.0.1 ${1:-7888}
+printf "d2:id1:12:op4:eval4:code1:ee" | $NC -v -N 127.0.0.1 ${1:-7888}
