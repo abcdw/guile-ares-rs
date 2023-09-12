271d35dd0070ed85f9cb1f2ddfe36fc073d6544e
Andrew Tropin <andrew@trop.in>
2023-09-12 12:17:54 +0400

    tests: integration: Use function instead of macro for setting up a server

--- tests/integration-test.scm
+++ tests/integration-test.scm
@@ -3,7 +3,11 @@
   #:use-module (nrepl client)
   #:use-module (bencode)
   #:use-module (ice-9 threads)
+  #:use-module (fibers conditions)
   #:use-module (srfi srfi-64))
+
+(use-modules (gider test-runners))
+(test-runner-current (test-runner-create))
 
 (define-syntax define-test
   (syntax-rules ()
@@ -12,21 +16,25 @@
        (define-public (test-name) e ...)
        (set-procedure-property! test-name 'srfi-64-test? #t)))))
 
+;; (call-with-nrepl-setup (lambda (client) (display "hi=======>\n")))
+
 ;; Hacky workaround for setting up and shutting down nrepl server and client
-(define-syntax with-nrepl-setup
-  (syntax-rules ()
-    ((_ client-socket e ...)
-     (let* ((nrepl-server (call-with-new-thread
-                           (lambda () (run-nrepl-server #:port 1134))))
-            (_ (sleep 1))
-            (client-socket (make-nrepl-client-socket #:port 1134))
-            (res e ...))
-       (when (port? client-socket)
-         (close-port client-socket))
-       (when (and (thread? nrepl-server) (not (thread-exited? nrepl-server)))
-         (cancel-thread nrepl-server)
-         (join-thread nrepl-server))
-       res))))
+(define* (call-with-nrepl-setup proc #:key (port 1134))
+  (let* ((server-started? (make-condition))
+         (nrepl-server (call-with-new-thread
+                        (lambda ()
+                          (run-nrepl-server
+                           #:port port
+                           #:started? server-started?))))
+         (_ (wait server-started?))
+         (client-socket (make-nrepl-client-socket #:port port)))
+    (call-with-values
+        (lambda () (proc client-socket))
+      (lambda vals
+        (close-port client-socket)
+        (cancel-thread nrepl-server)
+        (join-thread nrepl-server)
+        (apply values vals)))))
 
 (define (nrepl-eval-code client code-str)
   (scm->bencode
@@ -37,17 +45,19 @@
 
 ;; This a draft/stub for future implementation of integration tests
 (define-test simple-eval
-  (with-nrepl-setup
-   nrepl-client
-   (begin
-     (test-group "simple eval"
-       (test-group "arithmetics"
-         (test-equal "(+ 1 2 3)" "6"
-                     (nrepl-eval-code nrepl-client "(+ 1 2 3)"))))
-     (test-group "read error"
-       (test-group "arithmetics"
-         (test-equal "(+ 1 2 3)" "\"Couldn't read the code\""
-                     (nrepl-eval-code nrepl-client "(+ 1 2 3")))))))
+  (call-with-nrepl-setup
+   (lambda (nrepl-client)
+     (begin
+       (test-group "simple eval"
+         (test-group "arithmetics"
+           (test-equal "code for evaluation: (+ 1 2 3)"
+             "6"
+             (nrepl-eval-code nrepl-client "(+ 1 2 3)"))))
+       (test-group "read error"
+         (test-group "arithmetics"
+           (test-equal "code for evaluation: (+ 1 2 3"
+             "\"Couldn't read the code\""
+             (nrepl-eval-code nrepl-client "(+ 1 2 3"))))))))
 
 ;; TODO: Make it execute both calls properly
 ;; (define-public (tmp-fn)
@@ -58,5 +68,6 @@
 ;;   (with-nrepl-setup
 ;;    nrepl-client
 ;;    (nrepl-eval-code nrepl-client "(+ 1 2 3)")))
+
 
 ;; (simple-eval)
