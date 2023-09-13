f5895661fa67e7f3fd4e1f677cc6cfc6cb19337b
Andrew Tropin <andrew@trop.in>
2023-09-13 06:25:19 +0400

    tests: integration: Use test-utils

--- tests/integration-test.scm
+++ tests/integration-test.scm
@@ -4,21 +4,9 @@
   #:use-module (bencode)
   #:use-module (ice-9 threads)
   #:use-module (fibers conditions)
-  #:use-module (srfi srfi-64))
+  #:use-module (srfi srfi-64)
+  #:use-module (test-utils))
 
-(use-modules (gider test-runners))
-(test-runner-current (test-runner-create))
-
-(define-syntax define-test
-  (syntax-rules ()
-    ((_ test-name e ...)
-     (begin
-       (define-public (test-name) e ...)
-       (set-procedure-property! test-name 'srfi-64-test? #t)))))
-
-;; (call-with-nrepl-setup (lambda (client) (display "hi=======>\n")))
-
-;; Hacky workaround for setting up and shutting down nrepl server and client
 (define* (call-with-nrepl-setup proc #:key (port 1134))
   (let* ((server-started? (make-condition))
          (nrepl-server (call-with-new-thread
@@ -57,17 +45,32 @@
          (test-group "arithmetics"
            (test-equal "code for evaluation: (+ 1 2 3"
              "\"Couldn't read the code\""
-             (nrepl-eval-code nrepl-client "(+ 1 2 3"))))))))
+             (nrepl-eval-code nrepl-client "(+ 1 2 3"))))
 
-;; TODO: Make it execute both calls properly
-;; (define-public (tmp-fn)
-;;   (with-nrepl-setup
-;;    nrepl-client
-;;    (nrepl-eval-code nrepl-client "(+ 1 2 3)"))
-
-;;   (with-nrepl-setup
-;;    nrepl-client
-;;    (nrepl-eval-code nrepl-client "(+ 1 2 3)")))
-
-
-;; (simple-eval)
+;;        (test-group "simple interrupt workflow"
+;;          (scm->bencode
+;;           `(("op" . "clone"))
+;;           nrepl-client)
+;;          (define session-id
+;;            (assoc-ref (bencode->scm nrepl-client) "new-session"))
+;;          (test-assert "clone session" session-id)
+;;          (scm->bencode
+;;           `(("op" . "eval")
+;;             ("id" . "1")
+;;             ("session" . ,session-id)
+;;             ("code" . "(begin
+;; (+ 2 3)
+;; (sleep 1)
+;; 'evaluation-result)"))
+;;           nrepl-client)
+;;          (scm->bencode
+;;           `(("op" . "interrupt")
+;;             ("interrupt-id" . "1")
+;;             ("session" . ,session-id))
+;;           nrepl-client)
+;;          (test-equal "obtained interrupted status"
+;;            #("done" "interrupted")
+;;            (assoc-ref
+;;             (bencode->scm nrepl-client)
+;;             "status")))
+       ))))
