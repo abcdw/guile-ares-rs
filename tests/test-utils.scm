24adf866f4ac2049440b13eb50ffaf51f20e1b4c
Andrew Tropin <andrew@trop.in>
2023-09-13 06:23:45 +0400

    tests: Add test-utils module

---
+++ tests/test-utils.scm
@@ -0,0 +1,9 @@
+(define-module (test-utils)
+  #:export (define-test))
+
+(define-syntax define-test
+  (syntax-rules ()
+    ((_ test-name e ...)
+     (begin
+       (define-public (test-name) e ...)
+       (set-procedure-property! test-name 'srfi-64-test? #t)))))
