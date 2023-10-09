64ded47eef92864e647ec41ff7234988f4aaf036
Andrew Tropin <andrew@trop.in>
2023-10-09 08:11:42 +0400

    Change project name to guile-ares-rs

--- src/nrepl/extensions/session.scm
+++ src/nrepl/extensions/session.scm
@@ -1,21 +1,21 @@
-;;; guile-nrepl --- Asynchronous Reliable Extensible Scheme RPC Server
+;;; guile-ares-rs --- Asynchronous Reliable Extensible Scheme RPC Server
 ;;;
 ;;; Copyright © 2023 Andrew Tropin <andrew@trop.in>
 ;;;
-;;; This file is part of guile-nrepl.
+;;; This file is part of guile-ares-rs.
 ;;;
-;;; guile-nrepl is free software; you can redistribute it and/or modify it
+;;; guile-ares-rs is free software; you can redistribute it and/or modify it
 ;;; under the terms of the GNU General Public License as published by
 ;;; the Free Software Foundation; either version 3 of the License, or (at
 ;;; your option) any later version.
 ;;;
-;;; guile-nrepl is distributed in the hope that it will be useful, but
+;;; guile-ares-rs is distributed in the hope that it will be useful, but
 ;;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;;; General Public License for more details.
 ;;;
 ;;; You should have received a copy of the GNU General Public License
-;;; along with guile-nrepl.  If not, see <http://www.gnu.org/licenses/>.
+;;; along with guile-ares-rs.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (nrepl extensions session)
   #:use-module (fibers conditions)
