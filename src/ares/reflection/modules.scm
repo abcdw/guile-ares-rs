2936a0e0a01ce24c550a854477acf11ea8a2faef
Andrew Tropin <andrew@trop.in>
2024-01-09 13:13:50 +0300

    Add (ares reflection modules)

---
+++ src/ares/reflection/modules.scm
@@ -0,0 +1,53 @@
+;;; guile-ares-rs --- Asynchronous Reliable Extensible Sleek RPC Server
+;;;
+;;; Copyright © 2024 Andrew Tropin <andrew@trop.in>
+;;;
+;;; This file is part of guile-ares-rs.
+;;;
+;;; guile-ares-rs is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; guile-ares-rs is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with guile-ares-rs.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (ares reflection modules)
+  #:use-module (srfi srfi-1))
+
+(define (submodules mod)
+  "Return a list of submodules."
+  (hash-map->list (lambda (k v) v) (module-submodules mod)))
+
+(define (root-modules)
+  "Return a list of submodules of '() module."
+  (submodules (resolve-module '() #f)))
+
+(define* (all-child-modules mod #:optional (seen '()))
+  "Recursively traverse all the submodules of MOD and build a list out of
+it."
+  (let ((mod-submodules
+         (filter (lambda (m) (not (member m seen))) (submodules mod))))
+    (fold (lambda (m all) (append (all-child-modules m all) all))
+          (list mod)
+          mod-submodules)))
+
+(define (all-modules)
+  "Return a list of all modules."
+  (let* ((roots (root-modules))
+         (children (append-map all-child-modules roots)))
+    children))
+
+(define (modules-with-module-kind)
+  "Return a list of all modules, having a module-kind."
+  (define (module-with-module-kind m)
+    (and (module-kind m) m))
+  (let* ((guile-module (resolve-module '(guile)))
+         (roots (root-modules))
+         (children (append-map all-child-modules roots)))
+    (filter-map module-with-module-kind children)))
