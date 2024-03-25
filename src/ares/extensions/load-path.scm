578d5c5b018106a57d9baf25a4f4ebbb12513a88
Nikita Domnitskii <nikita@domnitskii.me>
2024-03-25 18:06:29 +0600

    extensions: Add load-path
    Signed-off-by: Andrew Tropin <andrew@trop.in>

---
+++ src/ares/extensions/load-path.scm
@@ -0,0 +1,51 @@
+;;; guile-ares-rs --- Asynchronous Reliable Extensible Sleek RPC Server
+;;;
+;;; Copyright © 2024 Nikita Domnitskii
+;;;
+;;; This file is part of guile-ares-rs.
+;;;
+;;; guile-ares-rs is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; guile-ares-rs is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with guile-ares-rs.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (ares extensions load-path)
+  #:export (load-path-extension))
+
+(define (get-load-path context)
+  (let ((reply (assoc-ref context 'reply))
+        (load-path (map (lambda (path)
+                          (or (false-if-exception
+                               (canonicalize-path path))
+                              path))
+                        %load-path)))
+    (reply
+     `(("status" . #("done"))
+       ("load-path" . ,(list->vector load-path))))))
+
+(define operations
+  `(("load-path" . ,get-load-path)))
+
+(define (wrap-load-path handler)
+  (lambda (context)
+    (let* ((message (assoc-ref context 'nrepl/message))
+           (op (assoc-ref message "op"))
+           (operation-function (assoc-ref operations op)))
+      (if operation-function
+          (operation-function context)
+          (handler context)))))
+
+(define load-path-extension
+  `((name . "ares/load-path")
+    (provides . (ares/load-path))
+    (requires . (nrepl/session))
+    (description . "Handles load-path related functionality.")
+    (wrap . ,wrap-load-path)))
