3fce75fd52632d602b4990f793ce29f7afd56cd3
Andrew Tropin <andrew@trop.in>
2024-05-01 17:45:14 +0300

    Remove rde from channels-lock.scm

--- rde/channels-lock.scm
+++ rde/channels-lock.scm
@@ -1,15 +1,4 @@
 (list (channel
-        (name 'rde)
-        (url "https://git.sr.ht/~abcdw/rde")
-        (branch "master")
-        (commit
-          "fbcd18a60219694dbd87f2e445baffef89341aa2")
-        (introduction
-          (make-channel-introduction
-            "257cebd587b66e4d865b3537a9a88cccd7107c95"
-            (openpgp-fingerprint
-              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
-      (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
