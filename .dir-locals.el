d2a9a391a2144ee446c14093f58babc7b33a90f2
Andrew Tropin <andrew@trop.in>
2025-04-25 14:13:46 +0700

    .dir-locals.el: Add test-case and test-suite identation

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((scheme-mode
+  .
+  ((indent-tabs-mode . nil)
+   (eval . (put 'test-suite 'scheme-indent-function 1))
+   (eval . (put 'test-case 'scheme-indent-function 1))
+   (eval . (put 'with-dynamic-state 'scheme-indent-function 1))
+   (eval . (put 'with-fluids 'scheme-indent-function 1)))))
