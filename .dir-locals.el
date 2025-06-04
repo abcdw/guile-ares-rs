f81bb3b72ed7af4944becd459c0b7b4d4c739048
Andrew Tropin <andrew@trop.in>
2025-06-04 11:32:08 +0700

    .dir-locals.el: Add identation rule for chain macro

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,5 +3,6 @@
   ((indent-tabs-mode . nil)
    (eval . (put 'test-suite 'scheme-indent-function 1))
    (eval . (put 'test 'scheme-indent-function 1))
+   (eval . (put 'chain 'scheme-indent-function 1))
    (eval . (put 'with-dynamic-state 'scheme-indent-function 1))
    (eval . (put 'with-fluids 'scheme-indent-function 1)))))
