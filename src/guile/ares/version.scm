19d7cfe78df936e074997d860ea8976d7b06bb9f
Noé Lopez <noelopez@free.fr>
2025-07-08 14:47:23 +0200

    Store current version in (ares version)

---
+++ src/guile/ares/version.scm
@@ -0,0 +1,11 @@
+;;; guile-ares-rs --- Asynchronous Reliable Extensible Sleek RPC Server
+;;;
+;;; Copyright © 2025 Libre en Communs <contact@a-lec.org>
+;;; Copyright © 2025 Noé Lopez <noelopez@free.fr>
+;;;
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+
+(define-module (ares version)
+  #:export (%ares-version))
+
+(define %ares-version "0.9.5")
