b311514891fa401e95ca28811a68bc95f190914d
Andrew Tropin <andrew@trop.in>
2025-08-01 06:49:17 +0700

    dev: Remove top-level SRFI-64 testing forms

--- dev/guile/2025-07-23-thread-interruption.scm
+++ dev/guile/2025-07-23-thread-interruption.scm
@@ -10,59 +10,60 @@
   #:use-module (srfi srfi-64)
   #:use-module (test-utils))
 
-(test-begin "")
+(define (run-test)
+  (test-begin "")
 
-(test-group "thread interruption"
-  (define thread
-    (call-with-new-thread
+  (test-group "thread interruption"
+    (define thread
+      (call-with-new-thread
+       (lambda ()
+         (let loop () (loop)))))
+
+    (test-assert (not (thread-exited? thread)))
+
+    (cancel-thread thread 'beep)
+
+    (test-equal 'beep (join-thread thread))
+    (test-assert (thread-exited? thread)))
+
+  (test-group "thread interruption doesnt trigger prompt"
+    (define tag (make-prompt-tag "ares-interrupt-tag"))
+    (define thread
+      (call-with-new-thread
+       (lambda ()
+         (call-with-prompt tag
+                           (lambda ()
+                             (let loop () (loop)))
+                           (lambda (kont)
+                             'aborted)))))
+
+    (test-assert (not (thread-exited? thread)))
+
+    (cancel-thread thread 'beep)
+
+    (test-equal 'beep (join-thread thread))
+    (test-assert (thread-exited? thread)))
+
+  (test-group "thread interruption to prompt"
+    (define tag (make-prompt-tag "ares-interrupt-tag"))
+    (define thread
+      (call-with-new-thread
+       (lambda ()
+         (call-with-prompt tag
+                           (lambda ()
+                             (let loop () (loop)))
+                           (lambda (kont)
+                             'aborted)))))
+
+    (test-assert (not (thread-exited? thread)))
+
+    (system-async-mark
      (lambda ()
-       (let loop () (loop)))))
+       (format #t "inside mark~%")
+       (abort-to-prompt tag))
+     thread)
 
-  (test-assert (not (thread-exited? thread)))
+    (test-equal 'aborted (join-thread thread))
+    (test-assert (thread-exited? thread)))
 
-  (cancel-thread thread 'beep)
-
-  (test-equal 'beep (join-thread thread))
-  (test-assert (thread-exited? thread)))
-
-(test-group "thread interruption doesnt trigger prompt"
-  (define tag (make-prompt-tag "ares-interrupt-tag"))
-  (define thread
-    (call-with-new-thread
-     (lambda ()
-       (call-with-prompt tag
-         (lambda ()
-           (let loop () (loop)))
-         (lambda (kont)
-           'aborted)))))
-
-  (test-assert (not (thread-exited? thread)))
-
-  (cancel-thread thread 'beep)
-
-  (test-equal 'beep (join-thread thread))
-  (test-assert (thread-exited? thread)))
-
-(test-group "thread interruption to prompt"
-  (define tag (make-prompt-tag "ares-interrupt-tag"))
-  (define thread
-    (call-with-new-thread
-     (lambda ()
-       (call-with-prompt tag
-         (lambda ()
-           (let loop () (loop)))
-         (lambda (kont)
-           'aborted)))))
-
-  (test-assert (not (thread-exited? thread)))
-
-  (system-async-mark
-   (lambda ()
-     (format #t "inside mark~%")
-     (abort-to-prompt tag))
-   thread)
-
-  (test-equal 'aborted (join-thread thread))
-  (test-assert (thread-exited? thread)))
-
-(test-end "")
+  (test-end ""))
