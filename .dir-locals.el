76f6e0f2ccf33e5f115497c61dae24dd91a1bf81
Andrew Tropin <andrew@trop.in>
2025-08-06 11:20:59 +0700

    .dir-locals: Add suite, suite-thunk, chain-and, test-group identation

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,8 +3,13 @@
   ((indent-tabs-mode . nil)
    (eval . (put 'test-suite-thunk 'scheme-indent-function 1))
    (eval . (put 'test-suite 'scheme-indent-function 1))
+   (eval . (put 'suite-thunk 'scheme-indent-function 1))
+   (eval . (put 'suite 'scheme-indent-function 1))
    (eval . (put 'test-thunk 'scheme-indent-function 1))
    (eval . (put 'test 'scheme-indent-function 1))
    (eval . (put 'chain 'scheme-indent-function 1))
+   (eval . (put 'chain-and 'scheme-indent-function 1))
+   ;; SRFI-64
+   (eval . (put 'test-group 'scheme-indent-function 1))
    (eval . (put 'with-dynamic-state 'scheme-indent-function 1))
    (eval . (put 'with-fluids 'scheme-indent-function 1)))))
