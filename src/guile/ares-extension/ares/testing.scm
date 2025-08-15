c7f5dde40c9edc7d0606498177a609f11519a124
Andrew Tropin <andrew@trop.in>
2025-08-15 15:15:15 +0700

    ares.testing: Add load-module-tests

--- src/guile/ares-extension/ares/testing.scm
+++ src/guile/ares-extension/ares/testing.scm
@@ -7,14 +7,17 @@
   #:export (ares.testing))
 
 (define (code->eval-request-context context code)
-  (cons `(nrepl/message . ,(append
-                            `(("op" . "eval")
-                              ("code" . ,code)
-                              ("ns" . "(ares suitbl ares)"))
-                            (alist-select-keys
-                             '("id" "session")
-                             (assoc-ref context 'nrepl/message))))
-   context))
+  (let ((nrepl-message (assoc-ref context 'nrepl/message)))
+    (cons `(nrepl/message . ,(append
+                              `(("op" . "eval")
+                                ("code" . ,code)
+                                ("ns" .
+                                 ,(or (assoc-ref nrepl-message "module")
+                                      "(ares suitbl ares)")))
+                              (alist-select-keys
+                               '("id" "session")
+                               nrepl-message)))
+          context)))
 
 (define (run context handler)
   "Run tests loaded into test runner."
@@ -31,8 +34,14 @@
             context
             "((@ (ares suitbl ares) get-current-test-runner-stats))")))
 
+(define (load-module-tests context handler)
+  "Load tests corresponding to current module."
+  (handler (code->eval-request-context
+            context
+            "((@ (ares suitbl ares) load-module-tests))")))
+
 (define (load-project-tests context handler)
-  "Return test runner stats."
+  "Load all project tests."
   (handler (code->eval-request-context
             context
             "((@ (ares suitbl ares) load-project-tests))")))
@@ -40,6 +49,7 @@
 (define operations
   `(("ares.testing/run" . ,run)
     ("ares.testing/get-test-runner-stats" . ,get-test-runner-stats)
+    ("ares.testing/load-module-tests" . ,load-module-tests)
     ("ares.testing/load-project-tests" . ,load-project-tests)))
 
 (define-with-meta (ares.testing handler)
