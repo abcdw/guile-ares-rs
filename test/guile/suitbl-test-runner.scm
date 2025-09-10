5eafaac4a98574b852201582a75dc050e81db147
Andrew Tropin <andrew@trop.in>
2025-09-10 11:47:56 +0700

    Update number of expected tests

--- test/guile/suitbl-test-runner.scm
+++ test/guile/suitbl-test-runner.scm
@@ -17,7 +17,7 @@
     (define number-of-tests
       (assoc-ref summary 'tests))
 
-    (unless (= 30 number-of-tests)
+    (unless (= 31 number-of-tests)
       (chain "Unexpected number of tests (~a), make sure all tests are executed and
 expected number of tests is up-to-date."
         (format #f _ number-of-tests)
