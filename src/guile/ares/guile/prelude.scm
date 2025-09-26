c3347157c1c9734cceffb39a209b3e41901b88a3
Andrew Tropin <andrew@trop.in>
2025-09-26 08:24:14 +0700

    Add alist helpers, threading macro and exception->string to prelude

--- src/guile/ares/guile/prelude.scm
+++ src/guile/ares/guile/prelude.scm
@@ -1,4 +1,18 @@
+;;; guile-ares-rs --- Asynchronous Reliable Extensible Sleek RPC Server
+;;;
+;;; Copyright © 2023, 2024, 2025 Andrew Tropin <andrew@trop.in>
+;;;
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+
 (define-module (ares guile prelude)
+  #:use-module (ares guile alist)
+  #:use-module (srfi srfi-197)
+  #:use-module (ares guile exceptions)
+  #:re-export (alist-select-keys
+               alist-cons
+               alist-delete
+               chain chain-and
+               exception->string)
   #:export (comment))
 
 (define-syntax comment
