cb25d85b0a0ce2d71db98c638db0ba2774cf238b
Noé Lopez <noelopez@free.fr>
2026-03-09 18:39:37 +0100

    Update to latest guix
    Default guile has been updated to 3.0.11, there is no need for
    guile-custom-ports anymore.

--- env/guix/ares/env/guix/channels.scm
+++ env/guix/ares/env/guix/channels.scm
@@ -14,7 +14,7 @@
          (url "https://git.guix.gnu.org/guix.git")
          (branch "master")
          (commit
-          "c1d307d80afca4a8efe1cd4e7455f5f04ed72406")
+          "97714ba894b55c5076c9e185602b51b4dfc25423")
          (introduction
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
