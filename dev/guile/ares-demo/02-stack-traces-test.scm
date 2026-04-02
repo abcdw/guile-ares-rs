c417fcf1ecd2f61d6d954c16a3c53c91de232a8e
Andrew Tropin <andrew@trop.in>
2026-04-02 09:33:40 +0700

    dev: Update tests for stack traces demo

--- dev/guile/ares-demo/02-stack-traces-test.scm
+++ dev/guile/ares-demo/02-stack-traces-test.scm
@@ -3,9 +3,22 @@
   #:use-module (ares guile prelude)
   #:use-module (ares-demo 02-stack-traces))
 
-(comment
- (define-suite kek
-   (test "7 div 2 = 3"
-     (is (= (some-fn 7) 3)))
-   (test "8 div 2 = 4"
-     (is (= (some-fn 8) 4)))))
+(define (fine-test)
+  (test "fine"
+    (is (even? 4))))
+
+(define-suite kek
+  'metadata
+  '((demo? . #t))
+
+  (test "another fine"
+    ;; (sleep 1)
+    (is 'ok))
+
+  (test "7 div 2 = 3"
+    (is (= (some-fn 7) 3)))
+
+  (test "8 div 2 = 4"
+    (is (= (some-fn 8) 4)))
+
+  (fine-test))
