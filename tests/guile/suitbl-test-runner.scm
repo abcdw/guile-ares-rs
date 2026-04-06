a7c8b355a9c8367871c8bcaee4a44b78fc805f53
Andrew Tropin <andrew@trop.in>
2026-04-06 11:39:55 +0700

    tests: Use junit test reporter instead of custom junit extraction

--- tests/guile/suitbl-test-runner.scm
+++ tests/guile/suitbl-test-runner.scm
@@ -2,7 +2,6 @@
   #:use-module (ares suitbl core)
   #:use-module (ares suitbl runners)
   #:use-module ((ares suitbl reporters) #:prefix reporters:)
-  #:use-module ((ares suitbl state) #:prefix state:)
   #:use-module (ares suitbl discovery)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-197)
@@ -49,12 +48,7 @@ expected number of tests is up-to-date."
 (define-public (run-project-tests-junit-output)
   (let* ((test-runner (make-suitbl-test-runner
                        #:config
-                       `((test-reporter . ,reporters:test-reporter-silent)))))
-    (define junit-xml
-      (parameterize ((test-runner* test-runner))
-        ((@ (ares suitbl ares) load-project-tests))
-        (test-runner `((type . runner/run-tests)))
-        (chain (test-runner `((type . runner/get-state)))
-          (state:get-suite-forest-with-summary _)
-          (reporters:forest->junit-xml _))))
-    (format #t "~a" junit-xml)))
+                       `((test-reporter . ,reporters:test-reporter-junit)))))
+    (parameterize ((test-runner* test-runner))
+      ((@ (ares suitbl ares) load-project-tests))
+      (test-runner `((type . runner/run-tests))))))
