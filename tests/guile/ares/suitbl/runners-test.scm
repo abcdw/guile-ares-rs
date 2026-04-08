ac76959104019401a83e128570d289cebd0e2d73
Andrew Tropin <andrew@trop.in>
2026-04-08 08:14:24 +0700

    suitbl: runners: Update copyright header

--- tests/guile/ares/suitbl/runners-test.scm
+++ tests/guile/ares/suitbl/runners-test.scm
@@ -1,5 +1,5 @@
 ;; SPDX-License-Identifier: GPL-3.0-or-later
-;; Copyright © 2025 Andrew Tropin <andrew@trop.in>
+;; SPDX-FileCopyrightText: 2025, 2026 Andrew Tropin <andrew@trop.in>
 
 (define-module (ares suitbl runners-test)
   #:use-module (ares suitbl core)
