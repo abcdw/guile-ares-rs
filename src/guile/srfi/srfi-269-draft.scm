5a62ff5d0bdca13ba1c4174eaae11da9653ba661
Andrew Tropin <andrew@trop.in>
2026-05-12 08:35:09 +0700

    Add srfi-269-draft

---
+++ src/guile/srfi/srfi-269-draft.scm
@@ -0,0 +1,140 @@
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; SPDX-FileCopyrightText: 2025, 2026 Andrew Tropin <andrew@trop.in>
+
+(define-library (srfi srfi-269-draft)
+  (cond-expand
+   ;; Without this import the module will be empty and eval in Ares
+   ;; won't work.
+   (guile (import (guile)))
+   (else (import (scheme base))))
+  (import (srfi 229))
+  (export test-runner*
+          set-test-runner!
+
+          is
+          test test?
+          test-thunk
+          suite suite?
+          suite-thunk suite-thunk?
+
+          define-suite)
+
+  (begin
+    (define (missing-test-runner message)
+      (error "test-runner* is not set" message))
+
+    (define test-runner*
+      (make-parameter missing-test-runner))
+
+    (define (set-test-runner! runner)
+      (let ((previous-runner (test-runner*)))
+        ;; For Scheme implementations not supporting setting of a parameter,
+        ;; the initial value can be an atomic box and this function can set the
+        ;; atomic box value instead of the parameter itself.
+        (test-runner* runner)
+        previous-runner))
+
+    (define (alist-contains? alist key)
+      (and (assoc key alist) #t))
+
+    (define (test? obj)
+      (and (list? obj)
+           (alist-contains? obj 'test/body-thunk)
+           (alist-contains? obj 'test/description)))
+
+    (define (suite? obj)
+      (and (list? obj)
+           (alist-contains? obj 'suite/body-thunk)
+           (alist-contains? obj 'suite/description)))
+
+    (define suite-thunk-tag-key (list 'suite-thunk?))
+
+    (define (make-suite-thunk-tag suite-entity)
+      (cons suite-thunk-tag-key suite-entity))
+
+    (define (suite-thunk? obj)
+      (and (procedure? obj)
+           (procedure/tag? obj)
+           (let ((tag (procedure-tag obj)))
+             (and (pair? tag)
+                  (eq? suite-thunk-tag-key (car tag))))))
+
+
+
+    (define-syntax is
+      (syntax-rules ()
+        ((_ (predicate argument ...))
+         ((test-runner*)
+          (list (cons 'type 'runner/run-assert)
+                (cons 'assertion
+                      (list
+                       (cons 'assertion/body-thunk
+                             (lambda () (predicate argument ...)))
+                       (cons 'assertion/args-thunk
+                             (lambda () (list argument ...)))
+                       (cons 'assertion/body
+                             (quote (predicate argument ...)))
+                       (cons 'assertion/location #f))))))
+        ((_ form)
+         ((test-runner*)
+          (list (cons 'type 'runner/run-assert)
+                (cons 'assertion
+                      (list
+                       (cons 'assertion/body-thunk
+                             (lambda () form))
+                       (cons 'assertion/body (quote form))
+                       (cons 'assertion/location #f))))))))
+
+    (define-syntax test-thunk
+      (syntax-rules (quote metadata)
+        ((_ test-description (quote metadata) metadata-value body ...)
+         (let ((test-entity
+                (list
+                 (cons 'test/body-thunk
+                       (lambda () body ... (if #f #f)))
+                 (cons 'test/body (quote (body ...)))
+                 (cons 'test/description test-description)
+                 (cons 'test/metadata metadata-value)
+                 (cons 'test/location #f))))
+           (lambda ()
+             ((test-runner*)
+              (list (cons 'type 'runner/load-test)
+                    (cons 'test test-entity))))))
+        ((_ test-description body ...)
+         (test-thunk test-description 'metadata '() body ...))))
+
+    (define-syntax test
+      (syntax-rules ()
+        ((_ test-description body ...)
+         ((test-thunk test-description body ...)))))
+
+    (define-syntax suite-thunk
+      (syntax-rules (quote metadata)
+        ((_ suite-description (quote metadata) metadata-value body ...)
+         (let ((suite-entity
+                (list
+                 (cons 'suite/body-thunk
+                       (lambda () body ... (if #f #f)))
+                 (cons 'suite/description suite-description)
+                 (cons 'suite/metadata metadata-value)
+                 (cons 'suite/location #f))))
+           (lambda/tag (make-suite-thunk-tag suite-entity)
+             ()
+             ((test-runner*)
+              (list (cons 'type 'runner/load-suite)
+                    (cons 'suite suite-entity))))))
+        ((_ suite-description body ...)
+         (suite-thunk suite-description 'metadata '() body ...))))
+
+    (define-syntax suite
+      (syntax-rules ()
+        ((_ suite-description body ...)
+         ((suite-thunk suite-description body ...)))))
+
+    ;; TODO: [Andrew Tropin, 2026-04-14] There is no define-public in
+    ;; r7rs, update SRFI?
+    (define-syntax define-suite
+      (syntax-rules ()
+        ((_ suite-name body ...)
+         (define suite-name
+           (suite-thunk (symbol->string 'suite-name) body ...)))))))
---
+++ tests/guile/srfi/srfi-269-draft-test.scm
@@ -0,0 +1,153 @@
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; SPDX-FileCopyrightText: 2026 Andrew Tropin <andrew@trop.in>
+
+(define-module (srfi srfi-269-draft-test)
+  #:use-module (srfi srfi-64)
+  #:use-module ((srfi srfi-269-draft) #:prefix t:))
+
+(define-syntax define-test
+  (syntax-rules ()
+    ((_ test-name e ...)
+     (begin
+       (define-public (test-name) e ...)
+       (set-procedure-property! test-name 'srfi-64-test? #t)))))
+
+(define (make-logging-runner)
+  (let ((events '()))
+    (lambda (message)
+      (let ((message-type (assoc-ref message 'type)))
+        (if (eq? message-type 'runner/get-log)
+            (reverse events)
+            (begin
+              (set! events (cons message events))
+              *unspecified*))))))
+
+(define (runner-events thunk)
+  (let ((runner (make-logging-runner)))
+    (parameterize ((t:test-runner* runner))
+      (thunk)
+      (runner '((type . runner/get-log))))))
+
+(define-test srfi-269
+  (test-group "srfi-269"
+    (test-assert "set-test-runner! returns the previous runner"
+      (let ((original-runner (t:test-runner*))
+            (new-runner (lambda (message) message))
+            (previous-runner #f)
+            (current-runner #f))
+        (dynamic-wind
+          (lambda () #t)
+          (lambda ()
+            (set! previous-runner (t:set-test-runner! new-runner))
+            (set! current-runner (t:test-runner*)))
+          (lambda ()
+            (t:set-test-runner! original-runner)))
+        (and (eq? original-runner previous-runner)
+             (eq? new-runner current-runner)
+             (eq? original-runner (t:test-runner*)))))
+
+    (test-assert "test? recognizes test entities"
+      (t:test? `((test/body-thunk . ,(lambda () #t))
+                 (test/description . "test"))))
+
+    (test-assert "suite? recognizes suite entities"
+      (t:suite? `((suite/body-thunk . ,(lambda () #t))
+                  (suite/description . "suite"))))
+
+    (test-group "is"
+      (let* ((events (runner-events
+                      (lambda ()
+                        (let ((x 41))
+                          (t:is (= 42 (+ x 1)))))))
+             (message (car events))
+             (assertion (assoc-ref message 'assertion)))
+        (test-equal "message type"
+          'runner/run-assert
+          (assoc-ref message 'type))
+        (test-equal "assertion body datum"
+          '(= 42 (+ x 1))
+          (assoc-ref assertion 'assertion/body))
+        (test-equal "assertion location"
+          #f
+          (assoc-ref assertion 'assertion/location))
+        (test-equal "body thunk value"
+          #t
+          ((assoc-ref assertion 'assertion/body-thunk)))
+        (test-equal "argument thunk values"
+          '(42 42)
+          ((assoc-ref assertion 'assertion/args-thunk)))))
+
+    (test-group "test"
+      (let* ((events (runner-events
+                      (lambda ()
+                        (t:test "addition"
+                          'metadata
+                          '((tag . unit))
+                          (define value 2)
+                          (t:is (= 4 (+ value value)))))))
+             (message (car events))
+             (test-entity (assoc-ref message 'test)))
+        (test-equal "message type"
+          'runner/load-test
+          (assoc-ref message 'type))
+        (test-equal "description"
+          "addition"
+          (assoc-ref test-entity 'test/description))
+        (test-equal "metadata"
+          '((tag . unit))
+          (assoc-ref test-entity 'test/metadata))
+        (test-equal "location"
+          #f
+          (assoc-ref test-entity 'test/location))
+        (test-assert "entity predicate"
+          (t:test? test-entity))
+        (test-assert "body thunk"
+          (procedure? (assoc-ref test-entity 'test/body-thunk)))
+        (let* ((body-events
+                (runner-events
+                 (lambda ()
+                   ((assoc-ref test-entity 'test/body-thunk)))))
+               (assertion (assoc-ref (car body-events) 'assertion)))
+          (test-equal "body thunk loads assertions"
+            '(= 4 (+ value value))
+            (assoc-ref assertion 'assertion/body)))))
+
+    (test-group "suite"
+      (let ((suite-loader
+             (t:suite-thunk "deferred"
+               'metadata
+               '((tag . suite))
+               (t:test "inside" #t))))
+        (test-assert "suite-thunk? recognizes suite thunks"
+          (t:suite-thunk? suite-loader))
+        (test-assert "suite-thunk? rejects ordinary procedures"
+          (not (t:suite-thunk? (lambda () #t))))
+        (let* ((events (runner-events (lambda () (suite-loader))))
+               (message (car events))
+               (suite-entity (assoc-ref message 'suite)))
+          (test-equal "message type"
+            'runner/load-suite
+            (assoc-ref message 'type))
+          (test-equal "description"
+            "deferred"
+            (assoc-ref suite-entity 'suite/description))
+          (test-equal "metadata"
+            '((tag . suite))
+            (assoc-ref suite-entity 'suite/metadata))
+          (test-equal "location"
+            #f
+            (assoc-ref suite-entity 'suite/location))
+          (test-assert "entity predicate"
+            (t:suite? suite-entity)))))
+
+    (test-group "define-suite"
+      (begin
+        (t:define-suite generated-suite
+          (t:test "inside generated suite" #t))
+        (test-assert "creates suite thunk"
+          (t:suite-thunk? generated-suite))
+        (let* ((events (runner-events (lambda () (generated-suite))))
+               (suite-entity (assoc-ref (car events) 'suite)))
+          (test-equal "generated description"
+            "generated-suite"
+            (assoc-ref suite-entity 'suite/description)))))))
