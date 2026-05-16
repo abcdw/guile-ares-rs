f1354a18f1f7c5201246b7868138537f023c0180
Andrew Tropin <andrew@trop.in>
2026-05-16 13:20:22 +0700

    Remove module definition from channels.scm

--- env/guix/ares/env/guix/channels.scm
+++ env/guix/ares/env/guix/channels.scm
@@ -4,21 +4,14 @@
 ;;;
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
 
-(define-module (ares env guix channels)
-  #:use-module (guix channels)
-  #:export (core-channels))
-
-(define core-channels
-  (list (channel
-         (name 'guix)
-         (url "https://git.guix.gnu.org/guix.git")
-         (branch "master")
-         (commit
-          "97714ba894b55c5076c9e185602b51b4dfc25423")
-         (introduction
-          (make-channel-introduction
-           "9edb3f66fd807b096b48283debdcddccfea34bad"
-           (openpgp-fingerprint
-            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))))
-
-core-channels
+(list (channel
+       (name 'guix)
+       (url "https://git.guix.gnu.org/guix.git")
+       (branch "master")
+       (commit
+        "97714ba894b55c5076c9e185602b51b4dfc25423")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
