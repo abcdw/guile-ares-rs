0a1cdf2362b9ca8a78b8ceadaf40071699f93eb2
Andrew Tropin <andrew@trop.in>
2026-07-22 09:28:25 +0700

    ares: Convert lookup files to SPDX headers

--- tests/guile/ares/nrepl/bootstrap-test.scm
+++ tests/guile/ares/nrepl/bootstrap-test.scm
@@ -1,21 +1,5 @@
-;;; guile-ares-rs --- Asynchronous Reliable Extensible Sleek RPC Server
-;;;
-;;; Copyright © 2023-2026 Andrew Tropin <andrew@trop.in>
-;;;
-;;; This file is part of guile-ares-rs.
-;;;
-;;; guile-ares-rs is free software; you can redistribute it and/or modify it
-;;; under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 3 of the License, or (at
-;;; your option) any later version.
-;;;
-;;; guile-ares-rs is distributed in the hope that it will be useful, but
-;;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;;; General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with guile-ares-rs.  If not, see <http://www.gnu.org/licenses/>.
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; SPDX-FileCopyrightText: 2023-2026 Andrew Tropin <andrew@trop.in>
 
 (define-module (ares nrepl bootstrap-test)
   #:use-module (ares ports)
